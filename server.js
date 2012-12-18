5ce2244353f0e273370fdd1e2030136499910854
nirmash <nirma@microsoft.com>
2012-12-18 00:59:45 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello, world in Azure!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+}).listen(process.env.PORT);  
