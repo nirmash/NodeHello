da903d46a78c33f4b7a50ca76df72bce9326712b
nirmash <nirma@microsoft.com>
2013-02-21 09:54:46 -0800

    Katal push

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello, world in Azure!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    res.end('<h1>Hello, world in Katal!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
