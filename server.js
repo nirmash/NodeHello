65b6935a2eb6fd69dea4ab0f78b7f0576641373f
nirmash <nirma@microsoft.com>
2013-04-04 11:25:13 -0700

    demo commit now

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello, world in WAWS using GitHub again!!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    res.end('<h1>Hello, world in WAWS for incubator folks and tech stars!!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
