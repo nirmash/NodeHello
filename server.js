43c24ccd2841edba866caf902c2b2f1725e4dd47
nirmash <nirma@microsoft.com>
2014-03-29 10:51:26 -0700

    dddd

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Good Morning, This is my demo with Node and Git!</h1><br>[helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    res.end('<h1>Good Morning, this should be less boring' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
