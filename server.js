5fc0d2ed7dbb4a78835b4f1b3ee35e659c8f3d04
nirmash <nirma@microsoft.com>
2014-04-17 10:40:56 -0700

    chnage #2

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Good Morning, this should be less boring' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
+    res.end('<h1>Good Morning, out of things to say  ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
 }).listen(process.env.PORT);  
