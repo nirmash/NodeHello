6841807fc8c5256a4dd24fd0e59163df0a1ecdf7
nirmash <nirma@microsoft.com>
2014-12-24 02:10:04 -0800

    in Israel

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Websites not in Hebrew 1,2,2!</h1>');
+    res.end('<h1>Hello Websites YES in Hebrew!</h1>');
 }).listen(process.env.PORT);  
