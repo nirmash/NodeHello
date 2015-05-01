fd98de52df683923d24f9d45ceb361eff221f2e6
nirma <nirmsk@hotmail.com>
2015-05-01 11:43:51 -0700

    one commit

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Build 2015 super fun! </h1>');
+    res.end('<h1>Hello Code and Azure! </h1>');
 }).listen(process.env.PORT);  
