1495c183f61505926d6388f7f43bcdc90b544838
nirmash <nirma@microsoft.com>
2016-12-06 10:35:35 +0200

    My Commit

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World App Service Web App not in hebrew!</h1>');
+    res.end('<h1>Hello World App Service Web App!</h1>');
 }).listen(process.env.PORT);  
