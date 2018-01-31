7a0981430d3b7a731e37a741abfd2fc4e3c57e1c
nirmash <nirma@microsoft.com>
2018-01-31 09:32:33 -0800

    clean up

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello App Service GM demo!</h1>');
+    res.end('<h1>Hello App Service demo!</h1>');
 }).listen(process.env.PORT);
